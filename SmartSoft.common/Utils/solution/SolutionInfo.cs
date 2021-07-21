698f119a80d7b4eec59973b96ed26d060d214cd2
Nightstars <ChristZhangowner@gmail..com>
2021-07-21 17:33:25 +0800

    add new features

---
+++ SmartSoft.common/Utils/solution/SolutionInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartSoft.common.Utils.solution
+{
+    public class SolutionInfo
+    {
+        /// <summary>
+        /// 项目名称
+        /// </summary>
+        public string projName { get; set; }
+
+        /// <summary>
+        /// 项目全名称
+        /// </summary>
+        public string projFullName { get; set; }
+    }
+}
---
+++ SmartSoft.common/Utils/solution/SolutionUtil.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartSoft.common.Utils.solution
+{
+    public class SolutionUtil
+    {
+        #region initialize
+        public string _solutionName { get; set; }
+        public SolutionUtil(string solutionName)
+        {
+            _solutionName = solutionName;
+        }
+        #endregion
+
+        #region parse
+        public List<SolutionInfo> SlnParse(){
+            List<SolutionInfo> list = new List<SolutionInfo>();
+            using FileStream fileStream = new FileStream(_solutionName, FileMode.Open, FileAccess.Read);
+            using StreamReader streamReader = new StreamReader(fileStream, Encoding.UTF8);
+            string linedata = null;
+            while ((linedata = streamReader.ReadLine()) != null)
+            {
+                if (linedata.Contains("Project(\""))
+                {
+                    string temp = linedata.Split(",")[1].Replace("\"", "").Replace("\\", "/").Trim();
+                    list.Add(new SolutionInfo {
+                        projName=temp.Substring(temp.LastIndexOf("/")).Substring(1),
+                        projFullName=temp
+                    });
+                }
+                else if (linedata.Contains("Global"))
+                {
+                    break;
+                }
+            }
+            return list;
+        }
+        #endregion
+
+    }
+}
