af052d9ec93b43d6b293aaeb5893eb398d93cb17
Nightstars <ChristZhangowner@gmail..com>
2021-07-23 14:44:49 +0800

    features optimized

--- SmartSoft.common/pub.bat
+++ SmartSoft.common/pub.bat
@@ -1,2 +1,2 @@
 nuget pack SmartSoft.common.csproj
-nuget push -Source http://192.168.2.114:10004/v3/index.json -ApiKey Ihavenoidea@0 SmartSoft.common.1.0.9.nupkg
+nuget push -Source http://192.168.2.114:10004/v3/index.json -ApiKey Ihavenoidea@0 SmartSoft.common.1.1.1.nupkg
